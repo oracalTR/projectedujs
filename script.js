5951e7039468afb71ab929bfd9d8e03a694e153f
Pavel Trushnikov <oracal@mail.ru>
2021-11-29 23:12:56 +0300

    Выполнение задания третьего урока

--- script.js
+++ script.js
@@ -1,10 +1,42 @@
 'use strict';
-const title = 'Projectedujs';
-let screens = 'Простые, Сложные, Интерактивные';
-let screenPrice = 1500;
+const title = AnwerService('Введите название проекта');
+let screens = AnwerService('Введите тип экрана', 'Пример: Простые, Сложные, Интерактивные');
+let screenPrice = parseInt(AnwerService('Сколько будет стоить данная работа?'));
 let rollback = 5;
-let fullPrice = 10000;
-let adaptive = true;
+let adaptive = confirm('Нужен ли адптив на сайте? Нажмите ОK для сайта с адаптивом.');
+let service1 = AnwerService('Какой дополнительный тип услуги нужен?');
+let servicePrice1 = parseInt(AnwerService('Сколько это будет стоить?')) || 0;
+let service2 = AnwerService('Какой дополнительный ещё тип услуги нужен?');
+let servicePrice2 = parseInt(AnwerService('Сколько это будет стоить?')) || 0;
+let fullPrice = screenPrice + servicePrice1 + servicePrice2 || 0;
+let servicePercentPrice = Math.ceil(fullPrice - (fullPrice * (rollback/100)));
+
+function AnwerService(textAnswer, placeHold) {
+    let answer = prompt(textAnswer, placeHold);
+    if(answer.trim()) {
+        return answer;
+    } else {
+        AnwerService(textAnswer);
+    }
+}
+
+console.log('servicePercentPrice:' , servicePercentPrice);
+
+if (fullPrice > 0 && fullPrice < 15000) {
+
+    console.log(`Стоимость сайта ${fullPrice} рублей - Скидка не предусмотрена`);
+
+} else if (fullPrice >= 15000 && fullPrice < 30000) {
+
+    console.log(`Стоимость сайта ${fullPrice} рублей - Даем скидку в 5%`);
+
+} else if (fullPrice >= 30000) {
+
+    console.log(`Стоимость сайта ${fullPrice} рублей - Даем скидку в 10%`);
+
+} else {
+    console.log(`Стоимость сайта ${fullPrice} Ошибка: Что то пошло не так!`);
+}
 
 console.log('title: ', typeof title);
 console.log('fullPrice: ', typeof fullPrice);
@@ -15,4 +47,4 @@ console.log('fullPrice: ', `${fullPrice} рублей`);
 console.log('Массив screens: ', screens.toLowerCase().split(','));
 console.log(`Процент отката дизайнеру за клиента ${fullPrice * (rollback/100)} рублей`);
 console.log('Изучаем JS');
-alert('Изучаем JS');
+//alert('Изучаем JS');
