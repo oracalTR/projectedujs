44a7a722b51c7aded491c11e0bbad9cbb5214460
Pavel Trushnikov <oracal@mail.ru>
2021-12-01 00:51:07 +0300

    Выполнение задания четвертого урока

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 'use strict';
-const title = anwerService('Введите название проекта');
+let title = anwerService('Введите название проекта');
 let screens = anwerService('Введите тип экрана', 'Пример: Простые, Сложные, Интерактивные');
 let screenPrice = parseInt(anwerService('Сколько будет стоить данная работа?'));
 let rollback = 5;
@@ -8,8 +8,9 @@ let service1 = anwerService('Какой дополнительный тип ус
 let servicePrice1 = parseInt(anwerService('Сколько это будет стоить?')) || 0;
 let service2 = anwerService('Какой дополнительный ещё тип услуги нужен?');
 let servicePrice2 = parseInt(anwerService('Сколько это будет стоить?')) || 0;
-let fullPrice = screenPrice + servicePrice1 + servicePrice2 || 0;
-let servicePercentPrice = Math.ceil(fullPrice - (fullPrice * (rollback/100)));
+let fullPrice = 0;
+let servicePercentPrice;
+let allServicePrices;
 
 function anwerService(textAnswer, placeHold) {
     let answer = prompt(textAnswer, placeHold);
@@ -20,25 +21,51 @@ function anwerService(textAnswer, placeHold) {
     }
 }
 
-console.log('servicePercentPrice:' , servicePercentPrice);
+const getAllServicePrices = function(service1, service2) {
+    allServicePrices = service1 + service2;
+};
 
-if (fullPrice > 0 && fullPrice < 15000) {
-    console.log(`Стоимость сайта ${fullPrice} рублей - Скидка не предусмотрена`);
-} else if (fullPrice >= 15000 && fullPrice < 30000) {
-    console.log(`Стоимость сайта ${fullPrice} рублей - Даем скидку в 5%`);
-} else if (fullPrice >= 30000) {
-    console.log(`Стоимость сайта ${fullPrice} рублей - Даем скидку в 10%`);
-} else {
-    console.log(`Стоимость сайта ${fullPrice} Ошибка: Что то пошло не так!`);
+function getFullPrice(price, service) {
+    fullPrice = price + service;
 }
 
-console.log('title: ', typeof title);
-console.log('fullPrice: ', typeof fullPrice);
-console.log('adaptive: ', typeof adaptive);
-console.log('screens: ', screens.length);
-console.log('screenPrice: ', screenPrice + ' рублей');
-console.log('fullPrice: ', `${fullPrice} рублей`);
+const getTitle = (text) => {
+    title = text.toUpperCase().trim().substr(0, 1) + text.toLowerCase().trim().substr(1);
+};
+
+const getServicePercentPrices = (price, donat) => {
+    servicePercentPrice = Math.ceil(price - (price * (donat/100)));
+};
+
+function getRollbackMessage(price) {
+    if (price > 0 && price < 15000) {
+        return `Стоимость сайта ${price} рублей - Скидка не предусмотрена`;
+    } else if (price >= 15000 && price < 30000) {
+        return `Стоимость сайта ${price} рублей - Даем скидку в 5%`;
+    } else if (price >= 30000) {
+        return `Стоимость сайта ${price} рублей - Даем скидку в 10%`;
+    } else {
+        return `Стоимость сайта ${price} Ошибка: Что то пошло не так!`;
+    }
+}
+
+const getDonateDezing = (price, donat) => {
+    return fullPrice * (rollback/100);
+};
+
+function showTypeOf(variable) {
+    return `${typeof variable}`;
+}
+
+getTitle(title);
+getAllServicePrices(servicePrice1, servicePrice2);
+getFullPrice(screenPrice, allServicePrices);
+getServicePercentPrices(fullPrice, rollback);
+
+
+console.log('title: ', showTypeOf(title));
+console.log('fullPrice: ', showTypeOf(fullPrice));
+console.log('adaptive: ', showTypeOf(adaptive));
 console.log('Массив screens: ', screens.toLowerCase().split(','));
-console.log(`Процент отката дизайнеру за клиента ${fullPrice * (rollback/100)} рублей`);
-console.log('Изучаем JS');
-//alert('Изучаем JS');
+console.log('сообщение о скидке пользователю: ', getRollbackMessage(fullPrice));
+console.log(`стоимость за вычетом процента отката посреднику ${servicePercentPrice} рублей`);
